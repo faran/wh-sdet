3b1652ac79fdc42db16e14bdfa5e802088eae037
Faran <faranster@gmail.com>
2021-03-01 13:30:24 +0000

    updating with coments

--- src/main/java/ReposService.java
+++ src/main/java/ReposService.java
@@ -7,6 +7,7 @@ import java.util.*;
 
 public class ReposService extends BaseServices{
 
+    /**Setting base path to Repos*/
     ReposService(){
         setBasePath("repos");
     }
@@ -19,6 +20,11 @@ public class ReposService extends BaseServices{
         response = reposRequest(pathParam1,pathParam2);
     }
 
+    /** We get list of json objects
+     * Each element in map is a single json object
+     * Map represents key and value combination
+     * List hold all the elements
+     * json path $ represents root object or array */
     public String latestReleaseTag() {
         List<Map<String, String>> jp = response.jsonPath().getList("$");
         return jp.get(0).get("name");
